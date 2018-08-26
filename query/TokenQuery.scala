1c0ad7679599960c8fb04c3f151dc29da2884f73
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-08-26 10:37:06 -0700

    add the beginnings of the codebase

---
+++ query/TokenQuery.scala
@@ -0,0 +1,26 @@
+package lex_query.query
+
+sealed trait TokenType
+
+// We can literally do Boyer-Moore string search on the tokens themselves, wouldn't that be absurd??
+case class TokenSequence(tokens: Seq[TokenType]) {
+  def append(t: TokenType): TokenSequence = TokenSequence(tokens :+ t)
+}
+
+object TokenSequence {
+  def init(t: TokenType): TokenSequence = TokenSequence(Seq(t))
+}
+
+// For symbol tokens. What passes for a "symbol" varies across languages.
+final case class Symbol(name: String) extends TokenType
+// Any indexing punctuation, including the '.' in ".<field>", and the square brackets in e.g.
+// "[<number>]" and "[<string>]".
+abstract class FieldExtractionOperator extends TokenType
+final case class DotOperator(field: Symbol) extends FieldExtractionOperator
+// TODO: something to find e.g. python_dict['asdf'] as an instance of dereferencing a field 'asdf'.
+final case class IndexOperator(arg: String) extends FieldExtractionOperator
+// Words like "def" in python, as well as e.g. assignments with "=". E.g. "var"/"val" in scala.
+final case class Definition(name: String) extends TokenType
+// This just records the mere fact that a function is invoked -- this usually happens with parens,
+// e.g. `myFun()` -- this just records the fact that there were open parens after an expression.
+final object FunctionCall extends TokenType
